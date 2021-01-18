6b38c947778217d0a4c7361ccf5ff81253e2e83d
kmj990929 <kmj990929@dgist.ac.kr>
2021-01-18 18:09:18 +0900

    feat: video modal 띄우는 기능 구현

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from login.models import *
 from mbti.calc import *
 import ast
 from .models import *
+from mbti.models import *
 
 # Create your views here.
 def index(request):
@@ -17,21 +18,28 @@ def index(request):
             mbti = ast.literal_eval(mbti)
             user.mbti = mbti
             user.save()
-            print(estimateArtistOnly(user.mbti))
 
             persona = settingPersona(user, True)
+            song_list = settingSongList(persona.artist)
 
-            content = {'userName':userName, 'mbti': mbti, 'artist_list':persona.artist}
+            content = {'userName':userName, 'mbti': mbti, 'artist_list':persona.artist, 'song_list':song_list}
             return render(request, 'home/home_mbti.html', content)
         except:
             if (user.mbti != ""):
                 # 이미 mbti 결과가 있는 경우
                 # mbti setting
-                mbti = ast.literal_eval(user.mbti)
+                mbti = user.mbti
+                if (type(mbti)==str):
+                    mbti = ast.literal_eval(mbti)
 
                 persona = settingPersona(user, False)
 
-                content = {'userName':userName, 'mbti' : mbti, 'artist_list':persona.artist}
+                artist = persona.artist
+                if (type(artist) == str):
+                    artist = ast.literal_eval(artist)
+                song_list = settingSongList(artist)
+
+                content = {'userName':userName, 'mbti' : mbti, 'artist_list':artist, 'song_list': song_list}
                 return render(request, 'home/home_mbti.html', content)
             else:
                 # 아직 mbti 결과가 없는 경우
@@ -50,7 +58,9 @@ def settingPersona(user, resetMbti):
         if (resetMbti):
             #artist 목록 추가
             persona.mbti = user.mbti
-            origin_artist_list = ast.literal_eval(persona.artist)
+            origin_artist_list = persona.artist
+            if (type(origin_artist_list) == str) :
+                origin_artist_list = ast.literal_eval(origin_artist_list)
             origin_artist_list += estimateArtistOnly(user.mbti)
             artist_list = list(set(origin_artist_list))
             persona.artist = artist_list
@@ -59,8 +69,20 @@ def settingPersona(user, resetMbti):
         return persona
     else:
         mbti = user.mbti
-        mbti = ast.literal_eval(mbti)
+        if (type(mbti) == str) :
+            mbti = ast.literal_eval(mbti)
+        
         artist = estimateArtistOnly(mbti)
         new_persona = Persona(userID = userID, mbti = mbti, artist = artist, songs = "[]", grade = "[]")
         new_persona.save()
         return new_persona
+
+def settingSongList(artist_list):
+    result_list = []
+    for artist in artist_list:
+        result = []
+        song_list = Song.objects.filter(artist = artist)
+        for song in song_list:
+            result.append(song)
+        result_list.append(result)
+    return result_list
