c5d82fef09389758d73d343b97b1edd48bc9ebc0
kmj990929 <kmj990929@dgist.ac.kr>
2021-01-18 16:08:39 +0900

    feat: result 데이터 추가

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    userID = models.TextField()
+    mbti = models.TextField()
+    artist = models.TextField()
+    songs = models.TextField()
+    grade = models.TextField()
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from login.models import *
 from mbti.calc import *
 import ast
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -11,18 +12,25 @@ def index(request):
         user = User.objects.filter(userName = userName)[0]
         try:
             # 방금 검사 결과 받아온 경우
+            # mbti setting
             mbti = request.GET['mbti_result']
             mbti = ast.literal_eval(mbti)
             user.mbti = mbti
             user.save()
             print(estimateArtistOnly(user.mbti))
+
+            settingPersona(user, True)
+
             content = {'userName':userName, 'mbti': mbti}
             return render(request, 'home/home_mbti.html', content)
         except:
             if (user.mbti != ""):
                 # 이미 mbti 결과가 있는 경우
+                # mbti setting
                 mbti = ast.literal_eval(user.mbti)
-                print(estimateArtistOnly(mbti))
+
+                settingPersona(user, False)
+
                 content = {'userName':userName, 'mbti' : mbti}
                 return render(request, 'home/home_mbti.html', content)
             else:
@@ -32,3 +40,23 @@ def index(request):
 
     else:
         return redirect('/login/')
+
+def settingPersona(user, resetMbti):
+    #persona setting하기
+    userID = user.userID
+    if (len(Persona.objects.filter(userID = userID))!= 0):
+        #이미 persona 존재
+        if (resetMbti):
+            #artist 목록 추가
+            persona = Persona.objects.filter(userID = userID)[0]
+            persona.mbti = user.mbti
+            origin_artist_list = ast.literal_eval(persona.artist)
+            origin_artist_list.append(estimateArtistOnly(user.mbti))
+            origin_artist_list = list(set(origin_artist_list))
+            persona.artist = origin_artist_list
+            persona.save()
+    else:
+        mbti = user.mbti
+        artist = estimateArtistOnly(mbti)
+        new_persona = Persona(userID = userID, mbti = mbti, artist = artist, songs = "[]", grade = "[]")
+        new_persona.save()
